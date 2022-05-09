ebbcd500d9617e037a573892b8e8ff1077ae5e2f
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-09 15:30:16 -0300

    Create script.js

---
+++ aula46/script.js
@@ -0,0 +1,21 @@
+function mostraHora() {
+    let data = new Date();
+  
+    return data.toLocaleTimeString('pt-BR', {
+      hour12: false
+    });
+  }
+  
+  const timer = setInterval(function() {
+    console.log(mostraHora());
+  }, 1000);
+  
+
+  setTimeout(function(){
+      clearInterval(timer);
+  },4000)
+
+
+  setTimeout(function() {
+      console.log('Hello world');
+  },1000)
