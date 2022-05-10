460b509b0cbd662d304c1769b7d832e436963bc3
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-10 10:40:44 -0300

    Create aula62.js

---
+++ aula62/aula62.js
@@ -0,0 +1,8 @@
+
+//shift remove indicice 0
+//push adiciona no final 
+
+const nomes = ['Eduardo','Monica','Joao','Bruno','Andre']
+const afterSlice = nomes.slice(2,4)
+console.log(afterSlice)
+// nomes[2]
