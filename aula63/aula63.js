35fcdd31b57373e227478853f11ec331d882b09f
Bruno Ramos <brunoramos.bsilva@gmail.com>
2022-05-10 10:52:58 -0300

    splice

---
+++ aula63/aula63.js
@@ -0,0 +1,11 @@
+
+const nomes = ['Bruno','Maria','Pedro','Eduardo','Monica'];
+
+
+
+//.splice(indice,delete,elem1,elem2)
+
+//pop
+const removidos = nomes.splice(3,2)
+
+console.log(nomes,removidos)
